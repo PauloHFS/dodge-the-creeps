acd8a42520ba5a1b5ea1a42ee11066e8d2532b7f
Paulo Hernane <paulohernane10@gmail.com>
2023-08-18 20:05:26 -0300

    first commit and start project

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@export var speed = 400 # pixels/sec
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Check for input.
+	# Move in the given direction.
+	# Play the appropriate animation.
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
