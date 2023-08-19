5c1a2cc7df188d468ffdcb12f65c2bb30a281545
Paulo Hernane <paulohernane10@gmail.com>
2023-08-18 23:08:09 -0300

    add hud

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400 # pixels/sec
 var screen_size
 
@@ -12,14 +14,14 @@ func _process(delta):
 	# Check for input.
 	# Move in the given direction.
 	# Play the appropriate animation.
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	
 	if velocity.length() > 0:
@@ -29,6 +31,23 @@ func _process(delta):
 		$AnimatedSprite2D.stop()
 
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	position = position.clamp(Vector2.ZERO, screen_size)
 	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred('disabled', true) # the engine wait to change this whem its safe
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
